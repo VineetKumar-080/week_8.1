d1c44368e381abbec3a5b2e5ba25068bc990d709
vineet <vineet8.4e8@gmail.com>
2025-04-14 21:54:29 +0530

    decoded.id

--- userLog.js
+++ userLog.js
@@ -1,1 +1,10 @@
 console.log("Checking the github codespace, is it working or not");
+
+// if(decoded){
+//     req.userId = decoded.id;
+//     next()
+// }else{
+//     res.status(403).json({
+//         message: "You are not signed in"
+//     })
+// }
